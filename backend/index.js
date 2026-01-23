c958e90d6c8af081509d118ef992f7f3a9332752
KrijanMainali <krijanmainali99@gmail.com>
2026-01-23 12:13:58 +0545

    created company controllers and company routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import config from "./config/config.js";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/comapny.route.js";
 
 
 
@@ -27,6 +28,7 @@ app.use(cors(corsOptions));
 const PORT =  config.PORT;
 
 app.use('/api/v1/user', userRoute);
+app.use('/api/v1/company',companyRoute);
 
 
 app.listen(PORT, ()=>{
