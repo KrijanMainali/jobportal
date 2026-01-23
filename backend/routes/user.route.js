38d692742dd67ad722541b713ebbee3007724349
KrijanMainali <krijanmainali99@gmail.com>
2026-01-23 11:22:33 +0545

    commit

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -8,13 +8,8 @@ const router = express.Router();
 
 router.route('/register').post(singleUpload,register);
 router.route('/login').post(login);
-<<<<<<< HEAD
 router.route('/profile/update').post(isAuthenticated,updateProfile);
 router.route('/logout').get(logout);
-=======
-router.route("/logout").get(logout);
-router.route('/profile/update').post(isAuthenticated,singleUpload,updateProfile);
->>>>>>> 2c76cc3acaa7e4114facc6b88591f14f62e4b5ef
 
 
 export default router;
