c29ab47493fe214fd2c64b3a8af6888574686470
KrijanMainali <krijanmainali99@gmail.com>
2026-01-25 08:57:32 +0545

    added some variables on config files

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  PORT : process.env.PORT || 3000 ,
-  SECRET_KEY : process.env.SECRET_KEY,
-  MONGO_URI :   process.env.MONGO_URI
+  PORT: process.env.PORT || 3000,
+  SECRET_KEY: process.env.SECRET_KEY,
+  MONGO_URI: process.env.MONGO_URI,
+  CLOUD_NAME:process.env.CLOUD_NAME,
+  API_KEY:process.env.API_KEY,
+  API_SECRET:process.env.API_KEY
 
 };
 
